eb89a7dc33f860cd8e1432f5db79a4b37f91f896
ErikHarutyunyan <000erik001@gmail.com>
2023-10-04 00:17:07 +0400

    404

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,30 @@
 // Styles
+import { Link } from "react-router-dom";
 import "./NotFoundPage.css";
+import { HOME } from "../../router/route-path";
 
 const NotFoundPage = () => {
-  return <section>NotFoundPage</section>;
+  return (
+    <section className="error">
+      <div className="container">
+        <div className="error_content">
+          <h1 className="error_title">404</h1>
+          <div className="error_info">
+            <h2 className="error_subtitle">Oops...</h2>
+            <p className="error_description">
+              We're sorry, but <br />
+              something went wrong.
+            </p>
+            <Link
+              to={HOME}
+              className="box-style btn-box d-center go_home theme_button">
+              Homepage
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundPage;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -42,24 +42,26 @@ import {
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path={HOME} element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path={GAMES} element={<Games />} />
-      <Route path={GAMES_SINGLE} element={<GameSingle />} />
-      <Route
-        path={ABOUT}
-        element={<About />}
-        // handle={{ scrollMode: "pathname" }}
-      ></Route>
-      <Route path={CONTACT} element={<Contact />} />
-      <Route path={SERVICES} element={<Services />} />
-      <Route path={NEWS} element={<News />} />
-      <Route path={NEWS_BLOG} element={<NewsSingle />} />
-      <Route path={PRIVACY} element={<PrivacyPolicy />} />
-      <Route path={CAREERS} element={<Careers />} />
-      <Route path={CAREERS_SINGLE} element={<CareersSingle />} />
+    <>
+      <Route path={HOME} element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path={GAMES} element={<Games />} />
+        <Route path={GAMES_SINGLE} element={<GameSingle />} />
+        <Route
+          path={ABOUT}
+          element={<About />}
+          // handle={{ scrollMode: "pathname" }}
+        ></Route>
+        <Route path={CONTACT} element={<Contact />} />
+        <Route path={SERVICES} element={<Services />} />
+        <Route path={NEWS} element={<News />} />
+        <Route path={NEWS_BLOG} element={<NewsSingle />} />
+        <Route path={PRIVACY} element={<PrivacyPolicy />} />
+        <Route path={CAREERS} element={<Careers />} />
+        <Route path={CAREERS_SINGLE} element={<CareersSingle />} />
+      </Route>
       <Route path={NOT_FOUND_PAGE} element={<NotFoundPage />} />
-    </Route>
+    </>
   )
 );
 
