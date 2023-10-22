d572a256e5987c99eddf7be1765ca8f8113c00fd
ErikHarutyunyan <000erik001@gmail.com>
2023-10-22 19:05:42 +0400

    bugsFix

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,7 +11,7 @@ import {
   CAREERS,
 } from "../../router/route-path";
 
-import logoSidebar from "../../assets/img/sungamin512.png";
+// import logoSidebar from "../../assets/img/sungamin512.png";
 
 const Sidebar = ({ sideBar = false, showHideSidebar = () => {} }) => {
   return (
@@ -30,12 +30,12 @@ const Sidebar = ({ sideBar = false, showHideSidebar = () => {} }) => {
             transition={{ type: "spring", bounce: 0, duration: 0.8 }}
             className={"sidebar_aside"}>
             <div className={"sidebar_wrapper"}>
-              <img
+              {/* <img
                 src={logoSidebar}
                 className="logoSidebar"
                 alt="logoSidebar"
                 fetchPriority="high"
-              />
+              /> */}
               <ul className="navbar-nav gap-4 py-4 py-lg-0 align-self-center ">
                 <motion.li
                   onClick={showHideSidebar}
